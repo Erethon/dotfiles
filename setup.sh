932612540afd01c96285b195f0de870de788e0c2
Dionysis Grigoropoulos <info@erethon.com>
2015-03-13 01:03:13 +0200

    setup: Remove newsbeuter entries

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 mkdir -p ~/.config
 mkdir -p ~/.vim/colors
-mkdir -p ~/.newsbeuter
 
 cp .gitconfig ~
 cp .tmux.conf ~
@@ -11,5 +10,4 @@ cp .bashrc ~
 cp .bash_aliases ~
 cp .config/ls_col ~/.config
 cp .vim/colors/* ~/.vim/colors
-cp .newsbeuter/* ~/.newsbeuter
 cp .Xdefaults ~/.Xdefaults
