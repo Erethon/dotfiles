2cf6d5b52c682056e02459025eb59ff30014748e
Dionysis Grigoropoulos <dgrig@erethon.com>
2016-02-09 03:44:41 +0200

    bin: Update battery script

--- bin/check-bat.sh
+++ bin/check-bat.sh
@@ -1,6 +1,10 @@
 WARN_LEVEL=$1
+FULL_LEVEL=$2
 CURRENT_LEVEL=$(cat /sys/class/power_supply/BAT0/uevent | grep CAPACITY= | cut -d '=' -f 2)
+STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ $CURRENT_LEVEL -lt $WARN_LEVEL ]; then
+if [ $CURRENT_LEVEL -lt $WARN_LEVEL ] && [ $STATUS = 'Discharging' ]; then
     /usr/bin/notify-send "Low Battery" "Charge level is at $CURRENT_LEVEL%"
+elif [ $CURRENT_LEVEL -ge $FULL_LEVEL ] && [ $STATUS = 'Charging' ]; then
+    /usr/bin/notify-send "Battery is charged" "Remove charger"
 fi
