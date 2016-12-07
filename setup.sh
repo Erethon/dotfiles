6571d0dc91f357bd5b08a814002d3bc7aa441a26
Dionysis Grigoropoulos <dgrig@erethon.com>
2016-12-07 18:43:47 +0200

    bash: Remove bash related files

--- setup.sh
+++ setup.sh
@@ -6,8 +6,6 @@ mkdir -p ~/.vim/colors
 cp .gitconfig ~
 cp .tmux.conf ~
 cp .vimrc ~
-cp .bashrc ~
-cp .bash_aliases ~
 cp .config/ls_col ~/.config
 cp .vim/colors/* ~/.vim/colors
 cp .Xdefaults ~/.Xdefaults
